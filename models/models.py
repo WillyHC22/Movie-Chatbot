ca25d0642cd9f5c4838a47549bb262189773aea1
WillyHC22 <willy_chung@hotmail.com>
2022-05-19 13:35:38 +0800

    added encoder decoder and attention module

---
+++ models/models.py
@@ -0,0 +1,149 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+PAD_token = 0
+SOS_token = 1
+EOS_token = 2
+
+class EncoderRNN(nn.Module):
+    def __init__(self, hidden_size, embedding, n_layers=1, dropout=0):
+        super(EncoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.embedding = embedding
+        self.n_layers = n_layers
+        self.dropout = dropout
+
+        self.gru = nn.GRU(input_size=hidden_size,
+                          hidden_size=hidden_size,
+                          num_layers=n_layers,
+                          dropout=(0 if n_layers == 1 else dropout),
+                          bidirectional=True)
+
+    def forward(self, input_seq, input_lengths, hidden=None):
+        embedded = self.embedding(input_seq)
+        packed = nn.utils.rnn.pack_padded_sequence(embedded, input_lengths)
+        outputs, hidden = self.gru(packed, hidden)
+        outputs, _ = nn.utils.rnn.pad_packed_sequence(outputs)
+        outputs = outputs[..., :self.hidden_size] + outputs[..., :self.hidden_size]
+        return outputs, hidden
+
+
+
+class Attention(nn.Module):
+
+    def __init__(self, method, hidden_size):
+        super(Attention, self).__init__()
+        self.method = method
+        if self.method not in ["dot", "general", "concat"]:
+            raise ValueError(self.method, "is not a appropriate method. Please pick either dot, general or concat")
+        self.hidden_size = hidden_size 
+        if self.method == "general":
+            self.attn = nn.Linear(self.hidden_size, hidden_size)
+        elif self.method == "concat":
+            self.attn = nn.Linear(self.hidden_size*2, hidden_size)
+            self.v = nn.Parameter(torch.FloatTensor(hidden_size))
+
+    def dot_score(self, hidden, encoder_output):
+        return torch.sum(hidden * encoder_output, dim=2)
+
+    def general_score(self, hidden, encoder_output):
+        energy = self.attn(encoder_output)
+        return torch.sum(hidden * energy, dim=2)
+
+    def concat_score(self, hidden, encoder_output):
+        energy = self.attn(torch.cat((hidden.expand(encoder_output.size(0), -1, -1), encoder_output), 2)).tanh()
+        return torch.sum(self.v * energy, dim=2)
+
+    def forward(self, hidden, encoder_outputs):
+        if self.method == 'general':
+            attn_energies = self.general_score(hidden, encoder_outputs)
+        elif self.method == 'concat':
+            attn_energies = self.concat_score(hidden, encoder_outputs)
+        elif self.method == 'dot':
+            attn_energies = self.dot_score(hidden, encoder_outputs)
+
+        attn_energies = attn_energies.t()
+        return F.softmax(attn_energies, dim=1).unsqueeze(1)
+
+
+class DecoderRNN(nn.Module):
+    def __init__(self, attn_model, embedding, hidden_size, output_size, n_layers=1, dropout=0.1):
+        super(DecoderRNN, self).__init__()
+
+        self.attn_model = attn_model
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.n_layers = n_layers
+        self.dropout = dropout
+
+        #layers
+        self.embedding = embedding
+        self.embedding_dropout = nn.Dropout(dropout)
+        self.gru = nn.GRU(input_size=hidden_size, 
+                          hidden_size=hidden_size, 
+                          num_layers=n_layers, 
+                          dropout=(0 if n_layers==1 else dropout))
+        self.concat = nn.Linear(hidden_size*2, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.attn = Attention(attn_model, hidden_size)
+
+    def forward(self, input_step, last_hidden, encoder_outputs):
+        embedded = self.embedding(input_step)
+        embedded = self.embedding_dropout(embedded)
+
+        rnn_output, hidden = self.gru(embedded, last_hidden)
+        attn_weights = self.attn(rnn_output, encoder_outputs)
+        context = attn_weights.bmm(encoder_outputs.transpose(0, 1))
+
+        rnn_output = rnn_output.squeeze(0)
+        context = context.squeeze(1)
+        concat_input = torch.cat((rnn_output, context), 1)
+        concat_output = torch.tanh(self.concat(concat_input))  
+
+        output = self.out(concat_output)
+        output = F.softmax(output, dim=1)
+
+        return output, hidden
+
+
+class GreedySearchDecoder(nn.Module):
+    def __init__(self, encoder, decoder):
+        super(GreedySearchDecoder, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, input_seq, input_length, max_length):
+        # Forward input through encoder model
+        encoder_outputs, encoder_hidden = self.encoder(input_seq, input_length)
+        # Prepare encoder's final hidden layer to be first hidden input to the decoder
+        decoder_hidden = encoder_hidden[:self.decoder.n_layers]
+        # Initialize decoder input with SOS_token
+        decoder_input = torch.ones(1, 1, device=device, dtype=torch.long) * SOS_token
+        # Initialize tensors to append decoded words to
+        all_tokens = torch.zeros([0], device=device, dtype=torch.long)
+        all_scores = torch.zeros([0], device=device)
+        # Iteratively decode one word token at a time
+        for _ in range(max_length):
+            # Forward pass through decoder
+            decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden, encoder_outputs)
+            # Obtain most likely word token and its softmax score
+            decoder_scores, decoder_input = torch.max(decoder_output, dim=1)
+            # Record token and score
+            all_tokens = torch.cat((all_tokens, decoder_input), dim=0)
+            all_scores = torch.cat((all_scores, decoder_scores), dim=0)
+            # Prepare current token to be next decoder input (add a dimension)
+            decoder_input = torch.unsqueeze(decoder_input, 0)
+        # Return collections of word tokens and scores
+        return all_tokens, all_scores
+
+
+def maskNLLLoss(inp, target, mask):
+    nTotal = mask.sum()
+    crossEntropy = -torch.log(torch.gather(inp, 1, target.view(-1, 1)).squeeze(1))
+    loss = crossEntropy.masked_select(mask).mean()
+    loss = loss.to(device)
+    return loss, nTotal.item()
+
